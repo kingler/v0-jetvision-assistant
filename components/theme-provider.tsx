041f9ba3061f17ce3ab295a5938815ddcdc404c1
Kingler Bercy <kingler@me.com>
2025-10-21 14:47:49 -0400

    fix(types): add children prop to ThemeProvider
    Green phase - extend ThemeProviderProps
    
    - Create custom ThemeProviderProps interface
    - Extend NextThemesProviderProps
    - Add explicit children: React.ReactNode
    
    Fixes: Property 'children' does not exist error
    
    Related to: TASK-000

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,8 +3,12 @@
 import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
+  type ThemeProviderProps as NextThemesProviderProps,
 } from 'next-themes'
+
+interface ThemeProviderProps extends NextThemesProviderProps {
+  children: React.ReactNode;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
